bc06e3f0e24927a5484d987239d5cd005a60512b
yoshiosan <i.am.a.passing.kamen.rider@gmail.com>
2024-10-22 19:53:00 +0900

    プログラム完成

--- SerialComm_Arduino_Sample.ino
+++ SerialComm_Arduino_Sample.ino
@@ -2,10 +2,17 @@
 
 void setup(void)
 {
-    
+    Serial.begin(9600);
 }
 
 void loop(void)
 {
-    
+    unsigned long second;
+    String message;
+
+    second = millis() / 1000;
+    message = String("起動から" + second);
+    message += "秒経過";
+    Serial.println(message);
+    delay(1000);
 }
